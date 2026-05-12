4e42485cf8c89bc704352c48c1a13eb4b7bc5492
EderMiqueias <edermiqueiasrodrigues@gmail.com>
2026-05-12 01:22:06 -0300

    add AwsS3Service & EmailService to tickets module providers

--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TicketsController } from './tickets.controller';
 import { TicketsService } from './tickets.service';
+import { AwsS3Service } from './utils/aws-s3.service';
+import { EmailService } from './utils/email.service';
 
 @Module({
   controllers: [TicketsController],
-  providers: [TicketsService],
+  providers: [TicketsService, AwsS3Service, EmailService],
 })
 export class TicketsModule {}
